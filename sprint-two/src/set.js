6013d3e52257c1ac8190fb1461d4a8c0bda21bb9
szhou1 <zhousteve3@gmail.com>
2016-09-15 09:26:32 -0700

    Completed set

--- sprint-two/src/set.js
+++ sprint-two/src/set.js
@@ -1,20 +1,30 @@
 var Set = function() {
   var set = Object.create(setPrototype);
-  set._storage = undefined;
+  set._storage = {};
   return set;
 };
 
 var setPrototype = {};
 
 setPrototype.add = function(item) {
+  if(!this.contains(item)){
+    this._storage[item] = true;
+  }
 };
 
 setPrototype.contains = function(item) {
+  return this._storage.hasOwnProperty(item);
 };
 
 setPrototype.remove = function(item) {
+  if(this.contains(item)){
+    delete this._storage[item];
+  }
 };
 
 /*
  * Complexity: What is the time complexity of the above functions?
  */
+// add - constant
+// contains - constant
+// remove - constant
