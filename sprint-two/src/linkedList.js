db81f1a7e68c9275ccc659755637c81c917fd983
szhou1 <zhousteve3@gmail.com>
2016-09-14 19:52:45 -0700

    Completed linked list

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -2,14 +2,34 @@ var LinkedList = function() {
   var list = {};
   list.head = null;
   list.tail = null;
-
   list.addToTail = function(value) {
+    var node = new Node(value);
+
+    if(list.tail === null){
+      list.head = node;
+      list.tail = node;
+      return;
+    } else {
+      list.tail.next = node;
+      list.tail = node;
+    }
   };
 
   list.removeHead = function() {
+    var head = list.head;
+    list.head = list.head.next;
+    return head.value;
   };
 
   list.contains = function(target) {
+    var node = list.head;
+    while(node !== null){
+      if(node.value === target){
+        return true;
+      }
+      node = node.next;
+    }
+    return false;
   };
 
   return list;
@@ -27,3 +47,6 @@ var Node = function(value) {
 /*
  * Complexity: What is the time complexity of the above functions?
  */
+ // addToTail() => O(1) constant
+ // removeHead() => O(1)
+ // contains() => O(n) linear
